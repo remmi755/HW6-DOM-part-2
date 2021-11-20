ee8a156964df7af76d43526ae21d3ed46178f4b3
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-11-20 17:02:34 +0300

    add something

--- scripts.js
+++ scripts.js
@@ -1,32 +1,19 @@
 const select = document.querySelector('#select')
 let current = document.querySelector('#input')
-const element = document.querySelector('div')
+const element = document.querySelector('#div')
 const btn = document.querySelector('#btn')
 
-select.addEventListener('change', function () {
-    element.style.backgroundColor = '#ffffff'
+select.addEventListener('input', function () {
+
+    element.className = this.value
+    // element.style.backgroundColor = '#ffffff'
        current.value = '#000000'
 
-    if (this.value === 'square'){
-        element.className = 'div1'
-
-    } else if (this.value === 'rectangle'){
-        element.className = 'div2'
-
-    } else if (this.value === 'circle') {
-       element.className = 'div3'
-    }
 });
 
 function changeColor() {
 
-    if (current.value !== '#000000') {
-
     element.style.backgroundColor = current.value
-
-} else {
-        element.style.backgroundColor = '#000000'
-    }
 }
 btn.addEventListener('click', changeColor)
 
