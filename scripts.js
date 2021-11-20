3eb6479f036c880299f74fbd2a4d50880a4c8cdf
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-11-20 19:18:37 +0300

    add some

--- scripts.js
+++ scripts.js
@@ -2,18 +2,19 @@ const select = document.querySelector('#select')
 let current = document.querySelector('#input')
 const element = document.querySelector('#div')
 const btn = document.querySelector('#btn')
+let defaultInput = '#000000'
+let defaultFigure = '#ffffff'
 
 select.addEventListener('input', function () {
-
     element.className = this.value
-    // element.style.backgroundColor = '#ffffff'
-       current.value = '#000000'
-
+    element.style.backgroundColor = defaultFigure
+    current.value = defaultInput
 });
 
 function changeColor() {
 
     element.style.backgroundColor = current.value
+
 }
 btn.addEventListener('click', changeColor)
 
